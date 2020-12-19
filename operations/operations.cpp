6919009769944a3140b23d6a4013bc224cdfe62d
Nakahotty <naskuuu@abv.bg>
2020-12-19 16:00:12 +0200

    Initialized module for operations (compose & other)

---
+++ operations/operations.cpp
@@ -0,0 +1,6 @@
+#ifndef __OPERATIONS_CPP
+#define __OPERATIONS_CPP
+
+#include "operations.h"
+
+#endif
---
+++ operations/operations.h
@@ -0,0 +1,9 @@
+#ifndef __OPERATIONS_H
+#define __OPERATIONS_H
+
+class Operations {
+public:
+private:
+};
+
+#endif
