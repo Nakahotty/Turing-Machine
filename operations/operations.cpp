aeb4cbf7f0db23fced203843de4294ff74e6b277
Nakahotty <naskuuu@abv.bg>
2021-01-04 12:52:24 +0200

    Implemented operations as class

---
+++ operations/operations.cpp
@@ -0,0 +1,16 @@
+#ifndef __OPERATIONS_CPP
+#define __OPERATIONS_CPP
+
+#include "operations.h"
+
+void Operations::composition() {
+    Machine m1,m2;
+}
+void Operations::decider() {
+
+}
+void Operations::loopOverMachine() {
+
+}
+
+#endif
---
+++ operations/operations.h
@@ -0,0 +1,14 @@
+#ifndef __OPERATIONS_H
+#define __OPERATIONS_H
+
+#include "../machine/machine.h"
+#include "../controller/controller.h"
+
+class Operations {
+public:
+    void composition();
+    void decider();
+    void loopOverMachine();
+};
+
+#endif
