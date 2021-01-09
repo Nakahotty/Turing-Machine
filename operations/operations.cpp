732f8010d6d069130b7e1cd0074f4e68c20e9b7f
Nakahotty <naskuuu@abv.bg>
2021-01-10 00:42:27 +0200

    Cleared comments from decider, writen logic for composition

--- operations/operations.cpp
+++ operations/operations.cpp
@@ -4,7 +4,9 @@
 #include "operations.h"
 
 void Operations::composition() {
-    // Machine m1,m2;
+    // Дадено: 2 машини - Т1, Т2; вход - А
+    // А = 11011 | Т1(А) -> 00000 | T2(T1(A)) -> XXXXX
+    // Композиция: done
 }
 void Operations::decider() {
     Machine decider("001101");
@@ -38,16 +40,6 @@ void Operations::decider() {
         std::cout << second.finishedSuccessfuly() << std:: endl;
         second.print();
     }
-
-    // decider has 2 transitions check if 1->1{halt}, if 0->0{reject}
-
-    // if (decider)
-    //     first with the same input 
-    // else 
-    //     second with the same input
-
-    // MachineController first("001101");  transitions -> transform everything in 1
-    // MachineController second("001101"); transitions -> transofrm everything in 0
 }
 
 void Operations::loopOverMachine() {
